e4f98faf8f1eb4f4e2c96a2e4ce6c096fda6127e
MAHMOUDAHMED175 <eng.mahmoudahmed176@gmail.com>
2023-09-18 00:02:43 +0300

    accounts_guide

--- lib/features/accounting/view/screens/accounts_guide.dart
+++ lib/features/accounting/view/screens/accounts_guide.dart
@@ -1,3 +1,5 @@
+import 'package:accounting/core/shared/widgets/defaulte_widget_button.dart';
+import 'package:accounting/core/shared/widgets/text_from_field_widget.dart';
 import 'package:accounting/core/utils/font_manager.dart';
 import 'package:accounting/core/utils/styles_manager.dart';
 import 'package:accounting/core/utils/values_manager.dart';
@@ -31,7 +33,7 @@ class AccountsGuide extends StatelessWidget {
                 child: Column(
                   children: [
                     SearchAccountsGuide(),
-                    Expanded(child: MyTreeView()),
+                    const Expanded(child: MyTreeView()),
                   ],
                 ),
               ),
@@ -40,20 +42,61 @@ class AccountsGuide extends StatelessWidget {
               flex: 3,
               child: Container(
                 decoration: BoxDecoration(
-                  color: ColorManager.grey100,
+                  // color: ColorManager.white,
                   border: Border.all(
                     color: ColorManager.grey, // Border color
                     width: 0.4, // Border width
                   ),
                 ),
-                child: ListView.separated(
-                  itemBuilder: (context, index) {
-                    return itemListViewAccountsGuide();
-                  },
-                  separatorBuilder: (context, index) {
-                    return const Divider();
-                  },
-                  itemCount: 20,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    HeaderDateTimeAccountsGuide(),
+                    Expanded(
+                      child: ListView.separated(
+                        itemBuilder: (context, index) {
+                          return itemListViewAccountsGuide();
+                        },
+                        separatorBuilder: (context, index) {
+                          return const Divider();
+                        },
+                        itemCount: 10,
+                      ),
+                    ),
+                    //no data
+                    Padding(
+                      padding: const EdgeInsets.all(AppPadding.p20),
+                      child: Text(
+                        'هذا الحساب فارغ',
+                        style: getBoldStyle(
+                            color: ColorManager.black, fontSize: FontSize.s20),
+                      ),
+                    ),
+                    DefaultWidgetButton(
+                      height: AppSize.s50,
+                      color: ColorManager.white,
+                      widget: Row(
+                        children: [
+                          Icon(
+                            Icons.add_circle_outline,
+                            color: ColorManager.blue,
+                            size: FontSize.s20,
+                          ),
+                          const SizedBox(
+                            width: AppSize.s12,
+                          ),
+                          Text(
+                            'اضف حساب',
+                            style: getBoldStyle(
+                                color: ColorManager.blue,
+                                fontSize: FontSize.s16),
+                          )
+                        ],
+                      ),
+                      onPress: () {},
+                      valueBorder: 0,
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -64,81 +107,182 @@ class AccountsGuide extends StatelessWidget {
   }
 }
 
-Widget itemListViewAccountsGuide() => Padding(
-      padding: EdgeInsets.all(AppPadding.p8),
-      child: Row(
-        children: [
-          Icon(
-            Icons.folder,
-            size: AppSize.s28,
-            color: ColorManager.blue,
-          ),
-          const SizedBox(
-            width: AppSize.s8,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'الاصول',
-                style: getBoldStyle(
-                    color: ColorManager.black, fontSize: FontSize.s18),
-              ),
-              const SizedBox(
-                height: AppSize.s8,
-              ),
-              Text(
-                '#1',
-                style: getBoldStyle(
-                  color: ColorManager.grey400,
-                ),
-              ),
-            ],
-          ),
-          const Spacer(),
-          Column(
-            children: [
-              Text(
-                '500',
-                style: getBoldStyle(
-                    color: ColorManager.black, fontSize: FontSize.s18),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                'مدين',
-                style: getBoldStyle(
-                  color: ColorManager.grey400,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            width: AppSize.s8,
-          ),
-          PopupMenuButton<String>(
-            child: Container(
-              width: AppSize.s32,
-              height: AppSize.s32,
-              color: ColorManager.blue200,
-              child: const Icon(Icons.more_horiz),
+Widget itemListViewAccountsGuide() => InkWell(
+      onTap: () {},
+      hoverColor: ColorManager.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(AppPadding.p8),
+        child: Row(
+          children: [
+            Icon(
+              Icons.folder,
+              size: AppSize.s28,
+              color: ColorManager.blue,
             ),
-            itemBuilder: (BuildContext context) {
-              return <PopupMenuEntry<String>>[
-                PopupMenuItem<String>(
-                  value: 'Item 1',
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.visibility,
-                      color: ColorManager.green,
-                    ),
-                    title: const Text('عرض'),
+            const SizedBox(
+              width: AppSize.s8,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'الاصول',
+                  style: getBoldStyle(
+                      color: ColorManager.black, fontSize: FontSize.s18),
+                ),
+                const SizedBox(
+                  height: AppSize.s8,
+                ),
+                Text(
+                  '#1',
+                  style: getBoldStyle(
+                    color: ColorManager.grey400,
                   ),
                 ),
-              ];
-            },
+              ],
+            ),
+            const Spacer(),
+            Column(
+              children: [
+                Text(
+                  '500',
+                  style: getBoldStyle(
+                      color: ColorManager.black, fontSize: FontSize.s18),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'مدين',
+                  style: getBoldStyle(
+                    color: ColorManager.grey400,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: AppSize.s8,
+            ),
+            PopupMenuButton<String>(
+              child: Container(
+                width: AppSize.s32,
+                height: AppSize.s32,
+                color: ColorManager.blue200,
+                child: const Icon(Icons.more_horiz),
+              ),
+              itemBuilder: (BuildContext context) {
+                return <PopupMenuEntry<String>>[
+                  PopupMenuItem<String>(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.edit,
+                        color: ColorManager.blue,
+                      ),
+                      title: const Text('تعديل'),
+                    ),
+                    onTap: () {},
+                  ),
+                  //if this sub
+                  PopupMenuItem<String>(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.delete,
+                        color: ColorManager.red,
+                      ),
+                      title: const Text('حذف'),
+                    ),
+                    onTap: () {},
+                  ),
+                ];
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+
+class HeaderDateTimeAccountsGuide extends StatelessWidget {
+  HeaderDateTimeAccountsGuide({super.key});
+
+  final TextEditingController dateTimeHeaderAccountsGuideController =
+      TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(
+        AppPadding.p8,
+      ),
+      child: Row(
+        children: [
+          InkWell(
+            child: Container(
+              height: 30,
+              width: 30,
+              color: ColorManager.blue200,
+              child: Icon(
+                Icons.keyboard_arrow_up,
+                color: ColorManager.black,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 1,
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: 30,
+              width: 30,
+              color: ColorManager.blue200,
+              child: Icon(
+                Icons.keyboard_arrow_down,
+                color: ColorManager.black,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 3,
+          ),
+          Expanded(
+            child: SizedBox(
+              height: AppSize.s32,
+              child: defaultFormField(
+                  controller: dateTimeHeaderAccountsGuideController,
+                  type: TextInputType.text,
+                  border: BorderRadius.circular(0),
+                  hintText: 'الفتره من / الى'),
+            ),
+          ),
+          const Spacer(),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: 30,
+              width: 30,
+              color: ColorManager.grey200,
+              child: Icon(
+                Icons.keyboard_arrow_right_outlined,
+                color: ColorManager.grey,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 2,
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: 30,
+              width: 30,
+              color: ColorManager.grey200,
+              child: Icon(
+                Icons.keyboard_arrow_left_outlined,
+                color: ColorManager.grey,
+              ),
+            ),
           ),
         ],
       ),
     );
+  }
+}
